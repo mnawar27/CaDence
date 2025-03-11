82cd1dea8a7cea291741ce945ac697a21d498b9a
Maisha Nawar <mnawar@Maishas-MacBook-Pro.local>
2025-03-10 20:41:22 -0400

    page config

--- streamlit_cadence.py
+++ streamlit_cadence.py
@@ -9,3 +9,15 @@ import wordcloud
 from wordcloud import WordCloud
 
 import numpy as np
+
+plt.style.use('dark_background')
+################################################### PAGE CONFIG
+st.set_page_config(
+    page_title="CaDence",
+    page_icon= "🎶",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
