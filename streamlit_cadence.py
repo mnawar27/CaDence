d1ed3654135bddc526e3968f1e50921e57827bad
Maisha Nawar <mnawar@Maishas-MacBook-Pro.local>
2025-03-10 20:44:04 -0400

    Dataframe Correlation

--- streamlit_cadence.py
+++ streamlit_cadence.py
@@ -21,3 +21,23 @@ st.set_page_config(
 alt.themes.enable("dark")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+################ Begin DF corelation to feed into Dashboard
+    df_seled_wk =pd.DataFrame(columns=['artist', 'song','duration','ts','sessionId','level','state','userAgent','userId','firstName','gender','week','time_zone'])
+#### Have 'present as default
+    if sb_wk==[]:
+        sb_wk=present
+    else:
+        sb_wk=sb_wk
+#### If no tz, then all tz to avoid errors before selections are made.
+    if sb_tz==[]:
+        sb_tz=allPlaces
+###### If selections are made, go with the selections
+    else:
+        sb_tz=sb_tz
+#### Now that there's a df with the right tz, narrow down to the ones with the right weeks we want    
+    for i in sb_tz:
+        select_tz= omega_raw.loc[omega_raw['time_zone'] == i]
+        df_seled_wk=pd.concat([df_seled_wk,select_tz])
+    df_selected_week= df_seled_wk[df_seled_wk['week'].isin(sb_wk)]
+
